12cbc427a3ed79919aa4e778078325da7cfff01f
Murilo <murilodesousag@gmail.com>
2022-11-18 13:36:46 -0300

    estilização basica

--- src/Componentes/Usuario/Usuario.js
+++ src/Componentes/Usuario/Usuario.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const User = styled.div`
+  border: black 1px solid;
+  margin-top: 10px;
+  width: 350px
+`
 function Usuario(props) {
   const [usuario, setUsuario] = useState(props.usuario);
   const [nome, setNome] = useState("");
@@ -7,9 +13,11 @@ function Usuario(props) {
   const [editar, setEditar] = useState(false);
 
   return (
-    <>
+    <User>
       {editar ? (
         <div>
+          <p>Nome:{usuario.name}</p>
+          <p>E-mail:{usuario.email}</p>
           <input value={nome} onChange={(e) => setNome(e.target.value)} />
           <input value={email} onChange={(e) => setEmail(e.target.value)} />
           <button>Enviar alterações</button>
@@ -22,7 +30,7 @@ function Usuario(props) {
       )}
       <button onClick={() => setEditar(!editar)}>Editar</button>
       <button>Excluir</button>
-    </>
+    </User>
   );
 }
 
